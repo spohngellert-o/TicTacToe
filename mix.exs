f7da171aa863a61e29671eecceec781edb6dc79c
spohngellert-o <spohngellert.o@northeastern.edu>
2023-08-14 13:53:01 -0400

    Adding dialyzer.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TicTacToe.MixProject do
     [
       app: :tic_tac_toe,
       version: "0.1.0",
-      elixir: "~> 1.15.4-otp-25",
+      elixir: "~> 1.14.5-otp-25",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       escript: [main_module: TicTacToe]
@@ -22,7 +22,8 @@ defmodule TicTacToe.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
